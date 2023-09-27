4537464914402556c96be5dcb1ec392e9d8923c4
Lamim <lemon07r@gmail.com>
2023-09-27 19:10:16 -0400

    updated packages + seo tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-modern-personal-website.netlify.app',
+  site: 'https://lamim.me',
   integrations: [mdx(), sitemap(), tailwind()]
 });
